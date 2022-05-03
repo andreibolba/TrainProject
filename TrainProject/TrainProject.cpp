dd6280a1cb3acc7029ecab3d1b3f98e8bcf3846e
Cimpianu Grigore-Adrian <cimpianuadi@gmail.com>
2022-05-03 20:37:01 +0300

    Added Mesh and Model classes
    Added Mesh and Model, to load models/objects, using assimp. Also added a Resource folder which contains a train object loaded for testing.

--- TrainProject/TrainProject.cpp
+++ TrainProject/TrainProject.cpp
@@ -3,13 +3,16 @@
 #include <math.h>
 
 #include <GL/glew.h>
-
+#include "Model.h"
+#include <filesystem>
 #include <glfw3.h>
 
 #include <GLM.hpp>
 #include <gtc/matrix_transform.hpp>
 #include <gtc/type_ptr.hpp>
-
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 #include "Camera.h"
 
 #define STB_IMAGE_IMPLEMENTATION
@@ -87,7 +90,13 @@ int main(int argc, char** argv)
 
     glewInit();
 
+    //stbi_set_flip_vertically_on_load(true);
+
     glEnable(GL_DEPTH_TEST);
+
+    std::filesystem::path localPath = std::filesystem::current_path();
+ 
+    Model ourModel(localPath.string() + "/Resources/train/electrictrain.obj");
 
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -108,88 +117,6 @@ int main(int argc, char** argv)
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    float vertices[] = {
-      0.0f,  0.0f, 0.5f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
-      0.5f, 0.0f, 0.5f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
-      0.5f, 0.5f, 0.5f,  0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
-      0.0f,  0.5f, 0.5f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f,
-      0.0f,  0.0f, 0.0f,  0.0f, 1.0f, 0.0f,   0.0f, 0.0f,
-      0.5f,  0.0f, 0.0f,  0.0f, 0.0f, 1.0f,   0.0f, 1.0f,
-      0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-      0.0f,  0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
-    };
-    unsigned int indices[] = {
-       0,1,2,
-       0,2,3,
-       1,5,6,
-       1,6,2,
-       5,4,7,
-       5,7,6,
-       4,0,3,
-       4,3,7,
-       0,5,1,
-       0,4,5,
-       3,2,6,
-       3,6,7
-    };
-
-    unsigned int VBO;
-    unsigned int VAO;
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
-    glGenBuffers(1, &VBO);
-    glGenVertexArrays(1, &VAO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    // texture coord attribute
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    glBindVertexArray(0);
-
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    int width, height, nrChannels;
-    std::string strFullExeFileName = argv[0];
-    std::string strExePath;
-    const size_t last_slash_idx = strFullExeFileName.rfind('\\');
-    if (std::string::npos != last_slash_idx) {
-        strExePath = strFullExeFileName.substr(0, last_slash_idx);
-    }
-    unsigned char* data = stbi_load((strExePath + "\\Bricks.jpg").c_str(), &width, &height, &nrChannels, 0);
-
-    if (data)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
-
     while (!glfwWindowShouldClose(window))
     {
         float currentFrame = static_cast<float>(glfwGetTime());
@@ -204,38 +131,27 @@ int main(int argc, char** argv)
 
         glUseProgram(shaderProgram);
 
-        glBindTexture(GL_TEXTURE_2D, texture);
-
-        glm::mat4 model = glm::mat4(1.0f);
-
-
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         // camera/view transformation
         glm::mat4 view = camera.GetViewMatrix();
 
         unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
         unsigned int projLoc = glGetUniformLocation(shaderProgram, "projection");
+        unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
         // pass them to the shaders (3 different ways)
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
         glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-        glBindVertexArray(VAO);
 
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-        unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-
-        glDrawElements(GL_TRIANGLES, 96, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
+        ourModel.Draw(shaderProgram);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
 
